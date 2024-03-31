669d1965703636382b1e91f292ac66ed4e4db2ff
Dannoopsy <belopolskikh.dd@phystech.edu>
2024-03-31 16:30:39 +0000

    initial configuration

---
+++ finetuning/train/train.py
@@ -0,0 +1,79 @@
+# +
+from datasets import load_dataset
+from transformers import ViTImageProcessor
+from transformers import ViTForImageClassification
+import torch
+import numpy as np
+from datasets import load_metric
+from transformers import TrainingArguments
+from transformers import Trainer
+
+def collate_fn(batch):
+    return {
+        'pixel_values': torch.stack([x['pixel_values'] for x in batch]),
+        'labels': torch.tensor([x['labels'] for x in batch])
+    }
+
+metric = load_metric("accuracy")
+def compute_metrics(p):
+    return metric.compute(predictions=np.argmax(p.predictions, axis=1), references=p.label_ids)
+
+ds = load_dataset('beans')
+def transform(example_batch):
+    # Take a list of PIL images and turn them to pixel values
+    inputs = processor([x for x in example_batch['image']], return_tensors='pt')
+
+    # Don't forget to include the labels!
+    inputs['labels'] = example_batch['labels']
+    return inputs
+prepared_ds = ds.with_transform(transform)
+
+labels = ds['train'].features['labels'].names
+model_name_or_path = 'google/vit-base-patch16-224-in21k'
+processor = ViTImageProcessor.from_pretrained(model_name_or_path)
+model = ViTForImageClassification.from_pretrained(
+    model_name_or_path,
+    num_labels=len(labels),
+    id2label={str(i): c for i, c in enumerate(labels)},
+    label2id={c: str(i) for i, c in enumerate(labels)}
+)
+
+training_args = TrainingArguments(
+  output_dir="./vit-base-beans",
+  per_device_train_batch_size=16,
+  evaluation_strategy="steps",
+  num_train_epochs=4,
+  fp16=True,
+  save_steps=100,
+  eval_steps=100,
+  logging_steps=10,
+  learning_rate=2e-4,
+  save_total_limit=2,
+  remove_unused_columns=False,
+  push_to_hub=False,
+  report_to='wandb',
+  load_best_model_at_end=True,
+)
+
+
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    data_collator=collate_fn,
+    compute_metrics=compute_metrics,
+    train_dataset=prepared_ds["train"],
+    eval_dataset=prepared_ds["validation"],
+    tokenizer=processor,
+)
+
+train_results = trainer.train()
+trainer.save_model()
+trainer.log_metrics("train", train_results.metrics)
+trainer.save_metrics("train", train_results.metrics)
+trainer.save_state()
+
+
+metrics = trainer.evaluate(prepared_ds['validation'])
+trainer.log_metrics("eval", metrics)
+trainer.save_metrics("eval", metrics)
