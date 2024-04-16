67a0b0a83adbd77303d6b8e49597ea9e91ee4f56
Dannoopsy <belopolskikh.dd@phystech.edu>
2024-04-16 20:17:07 +0000

    added hydra

---
+++ finetuning/train/main.py
@@ -0,0 +1,13 @@
+import hydra
+from omegaconf import DictConfig
+
+from train import train
+
+
+@hydra.main(config_path="../../configs", config_name="config", version_base="1.3")
+def main(cfg: DictConfig) -> None:
+    train(cfg)
+
+    
+if __name__ == "__main__":
+    main()
--- finetuning/train/train.py
+++ finetuning/train/train.py
@@ -8,72 +8,80 @@ from datasets import load_metric
 from transformers import TrainingArguments
 from transformers import Trainer
 
+
+
 def collate_fn(batch):
     return {
         'pixel_values': torch.stack([x['pixel_values'] for x in batch]),
         'labels': torch.tensor([x['labels'] for x in batch])
     }
-
-metric = load_metric("accuracy")
-def compute_metrics(p):
+def compute_metrics(p, metric):
     return metric.compute(predictions=np.argmax(p.predictions, axis=1), references=p.label_ids)
-
-ds = load_dataset('beans')
-def transform(example_batch):
+def transform(example_batch, processor):
     # Take a list of PIL images and turn them to pixel values
     inputs = processor([x for x in example_batch['image']], return_tensors='pt')
 
     # Don't forget to include the labels!
     inputs['labels'] = example_batch['labels']
     return inputs
-prepared_ds = ds.with_transform(transform)
-
-labels = ds['train'].features['labels'].names
-model_name_or_path = 'google/vit-base-patch16-224-in21k'
-processor = ViTImageProcessor.from_pretrained(model_name_or_path)
-model = ViTForImageClassification.from_pretrained(
-    model_name_or_path,
-    num_labels=len(labels),
-    id2label={str(i): c for i, c in enumerate(labels)},
-    label2id={c: str(i) for i, c in enumerate(labels)}
-)
-
-training_args = TrainingArguments(
-  output_dir="./vit-base-beans",
-  per_device_train_batch_size=16,
-  evaluation_strategy="steps",
-  num_train_epochs=4,
-  fp16=True,
-  save_steps=100,
-  eval_steps=100,
-  logging_steps=10,
-  learning_rate=2e-4,
-  save_total_limit=2,
-  remove_unused_columns=False,
-  push_to_hub=False,
-  report_to='wandb',
-  load_best_model_at_end=True,
-)
 
 
+def train(cfg):
+    model_name = cfg.model.model_name
+    dset_name = cfg.data.dset_name
+    metric_name = cfg.training.metric_name
+    output_dir = cfg.data.output_dir
+    batch_size = cfg.training.batch_size
+    epochs = cfg.training.epochs
+    learning_rate = cfg.training.learning_rate
+    
+    metric = load_metric(metric_name)
+    ds = load_dataset(dset_name)
+    prepared_ds = ds.with_transform(lambda x :transform(x, processor))
 
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    data_collator=collate_fn,
-    compute_metrics=compute_metrics,
-    train_dataset=prepared_ds["train"],
-    eval_dataset=prepared_ds["validation"],
-    tokenizer=processor,
-)
+    labels = ds['train'].features['labels'].names
+    processor = ViTImageProcessor.from_pretrained(model_name)
+    
+    model = ViTForImageClassification.from_pretrained(
+        model_name,
+        num_labels=len(labels),
+        id2label={str(i): c for i, c in enumerate(labels)},
+        label2id={c: str(i) for i, c in enumerate(labels)}
+    )
 
-train_results = trainer.train()
-trainer.save_model()
-trainer.log_metrics("train", train_results.metrics)
-trainer.save_metrics("train", train_results.metrics)
-trainer.save_state()
+    training_args = TrainingArguments(
+      output_dir=output_dir,
+      per_device_train_batch_size=batch_size,
+      evaluation_strategy="steps",
+      num_train_epochs=epochs,
+      fp16=True,
+      save_steps=100,
+      eval_steps=100,
+      logging_steps=10,
+      learning_rate=learning_rate,
+      save_total_limit=2,
+      remove_unused_columns=False,
+      push_to_hub=False,
+      report_to='wandb',
+      load_best_model_at_end=True,
+    )
 
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        data_collator=collate_fn,
+        compute_metrics=lambda x :compute_metrics(x, metric),
+        train_dataset=prepared_ds["train"],
+        eval_dataset=prepared_ds["validation"],
+        tokenizer=processor,
+    )
 
-metrics = trainer.evaluate(prepared_ds['validation'])
-trainer.log_metrics("eval", metrics)
-trainer.save_metrics("eval", metrics)
+    train_results = trainer.train()
+    trainer.save_model()
+    trainer.log_metrics("train", train_results.metrics)
+    trainer.save_metrics("train", train_results.metrics)
+    trainer.save_state()
+
+    metrics = trainer.evaluate(prepared_ds['validation'])
+    trainer.log_metrics("eval", metrics)
+    trainer.save_metrics("eval", metrics)
